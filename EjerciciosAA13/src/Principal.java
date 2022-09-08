47c7750873e21eb3772b46b73848c59d91aa057e
David <David@DESKTOP-FABJJN4.home>
2022-09-08 16:48:48 +0200

    ddd

--- EjerciciosAA13/src/Principal.java
+++ EjerciciosAA13/src/Principal.java
@@ -1,9 +1,49 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hola");
+
+		File documento = new File("Peliculas_11_20.txt");
+		
+		try {
+			String peliculas="";
+			Scanner sc = new Scanner(documento);
+			
+			while(sc.hasNextLine()) {
+				
+			peliculas +=sc.nextLine() + "\n";
+			
+			}
+			System.out.println(peliculas);
+			sc.close();
+		} catch(FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
+
+		File documentos = new File("Peliculas_1_10.txt");
+		
+		try {
+			String peliculas="";
+			Scanner sc = new Scanner(documento);
+			
+			while(sc.hasNextLine()) {
+				
+			peliculas +=sc.nextLine() + "\n";
+			
+			}
+			System.out.println(peliculas);
+			sc.close();
+		} catch(FileNotFoundException e) {
+			
+			e.printStackTrace();
+		}
+		
 		
 
 	}
